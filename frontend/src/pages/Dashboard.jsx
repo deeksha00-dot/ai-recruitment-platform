5dd503076cf01553a236ec930c764b4b9ae6f352
Deeksha Bhat <deekshabhat506@gmail.com>
2026-07-11 21:57:29 +0530

    Remove pycache and add gitignore

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard() {
+    return (
+        <div>
+            <h1>AI Recruitment Platform</h1>
+
+            <h3>Dashboard</h3>
+        </div>
+    );
+}
+
+export default Dashboard;
